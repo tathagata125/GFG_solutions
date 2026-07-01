17d5ea3455ecd3188dc58a845438b461dc77cba7
tathagata125 <144534128+tathagata125@users.noreply.github.com>
2026-07-01 20:44:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,17 +1,19 @@
 class Solution {
-    int ways(int w, int val[], int wt[], int idx, int[][] mem) {
-        if(w == 0 || idx < 0)return 0;
-        if(mem[w][idx] != 0)return mem[w][idx];
-        int notTake = ways(w,val,wt,idx-1,mem);
-        if(w-wt[idx] < 0)return notTake;
-        int take = ways(w-wt[idx],val,wt,idx-1,mem)+val[idx];
-        
-        return mem[w][idx] = Math.max(notTake,take);
-        
-    }
+    
     public int knapsack(int W, int val[], int wt[]) {
         // code here
-        int[][] mem = new int[W+1][val.length];
-       return ways(W, val, wt,val.length-1, mem);
+        int[][] mem = new int[W+1][val.length+1];
+        
+        for(int i = 1;i<=W;i++){
+            for(int j = 1; j<=val.length;j++){
+                if(i-wt[j-1] >= 0){
+                    mem[i][j] = Math.max(mem[i][j-1],mem[i-wt[j-1]][j-1]+val[j-1]);
+                }
+                else{
+                    mem[i][j] = mem[i][j-1];
+                }
+            }
+        }
+        return mem[W][val.length];
     }
 }
